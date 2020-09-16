2cd6ca294c9dd07b848cbcce527440a7799190c7
Luke Esworthy <lukesworthy@gmail.com>
2020-09-16 13:44:35 -0500

    auth set up, creating PR before moving to forms

--- setlisttrackerapp/views/__init__.py
+++ setlisttrackerapp/views/__init__.py
@@ -1,3 +1,4 @@
 from .home import home
+from .auth.logout import logout_user
 from .songs.list import song_list
 from .events.list import event_list
--- setlisttrackerapp/views/songs/list.py
+++ setlisttrackerapp/views/songs/list.py
@@ -1,9 +1,11 @@
 import sqlite3
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from setlisttrackerapp.models import Song
 from ..connection import Connection
 
 
+@login_required
 def song_list(request):
     if request.method == 'GET':
         with sqlite3.connect(Connection.db_path) as conn:
