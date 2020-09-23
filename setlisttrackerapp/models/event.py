6ab803c96ccc802b9e235b82b0e7b6f7f2d82c2c
Luke Esworthy <lukesworthy@gmail.com>
2020-09-23 18:59:40 -0500

    commit after making some changes to event model

--- setlisttrackerapp/models/event.py
+++ setlisttrackerapp/models/event.py
@@ -3,14 +3,14 @@ from django.contrib.auth.models import User
 
 
 class Event(models.Model):
-    name = models.CharField(max_length=75)
+    name = models.CharField(blank=False, max_length=75)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.DateField()
-    start_time = models.TimeField()
-    end_time = models.TimeField()
-    location = models.CharField(max_length=100)
-    duration = models.IntegerField()
-    notes = models.CharField(max_length=500)
+    date = models.DateField(blank=False)
+    start_time = models.TimeField(blank=False)
+    end_time = models.TimeField(blank=False)
+    location = models.CharField(blank=False, max_length=100)
+    duration = models.IntegerField(blank=False)
+    notes = models.CharField(blank=False, max_length=500)
 
     class Meta:
         verbose_name = ("event")
