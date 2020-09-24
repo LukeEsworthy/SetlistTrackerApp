a11f03850743fc1f4afb2ca877998e9764e94946
Luke Esworthy <lukesworthy@gmail.com>
2020-09-23 22:22:04 -0500

    commit after computer crashed

--- setlisttrackerapp/views/events/list.py
+++ setlisttrackerapp/views/events/list.py
@@ -2,6 +2,7 @@ import sqlite3
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 from setlisttrackerapp.models import Event
 from ..connection import Connection
 
